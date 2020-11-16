7c7b1ef46c6e9b6076534505375472b2c3926b0f
kolychka <randytaylor.fckoff@gmail.com>
2020-11-17 00:41:05 +0500

    Added inital struct

---
+++ index.js
@@ -0,0 +1,7 @@
+import './styles/style.scss'
+import './assets/fontawesome-free-5.15.1-web/css/all.min.css'
+import data from './assets/data.json'
+
+window.onload = function () {
+    let catalog = document.getElementsByClassName('catalog');
+    for (let i = 0; i < d
